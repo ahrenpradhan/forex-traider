4f5e553b7c0b8643951b4181e7677a9cb9426fe8
Ahren Pradhan <ahrenpradhan@gmail.com>
2020-03-26 21:22:03 +0530

    v1.0.2

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -4,94 +4,204 @@ import OrderHeader from './OrderHeader';
 export default class OrderList extends React.Component{
     state = {
         page: "OpenOrder",
+        currentPage : 0,
+        maxPage : null,
         fetchedData : [
             {
                 Name : 'abc',
                 Qty : 1,
                 AvgPrice : '1$',
                 PL : 1,
-                Status : 'open'
+                Status : 'open',
+                Date : '1'
             },{
                 Name : 'abc',
                 Qty : 1,
                 AvgPrice : '1$',
                 PL : 1,
-                Status : 'open'
+                Status : 'open',
+                Date : '1'
             },{
                 Name : 'abc',
                 Qty : 1,
                 AvgPrice : '1$',
                 PL : 1,
-                Status : 'open'
+                Status : 'open',
+                Date : '1'
             },{
                 Name : 'abc',
                 Qty : 1,
                 AvgPrice : '1$',
                 PL : 1,
-                Status : 'open'
+                Status : 'open',
+                Date : '1'
             },{
                 Name : 'abc',
                 Qty : 1,
                 AvgPrice : '1$',
                 PL : 1,
-                Status : 'open'
+                Status : 'open',
+                Date : '1'
             },{
                 Name : 'abc',
                 Qty : 1,
                 AvgPrice : '1$',
                 PL : 1,
-                Status : 'open'
+                Status : 'open',
+                Date : '1'
             },{
                 Name : 'abc',
                 Qty : 1,
                 AvgPrice : '1$',
                 PL : 1,
-                Status : 'open'
+                Status : 'open',
+                Date : '1'
             },{
                 Name : 'abc',
                 Qty : 1,
                 AvgPrice : '1$',
                 PL : 1,
-                Status : 'open'
+                Status : 'open',
+                Date : '1'
             },{
                 Name : 'abc',
                 Qty : 1,
                 AvgPrice : '1$',
                 PL : 1,
-                Status : 'open'
+                Status : 'open',
+                Date : '1'
             },{
                 Name : 'abc',
                 Qty : 1,
                 AvgPrice : '1$',
                 PL : 1,
-                Status : 'open'
+                Status : 'open',
+                Date : '1'
             },{
                 Name : 'abc',
                 Qty : 1,
                 AvgPrice : '1$',
                 PL : 1,
-                Status : 'open'
+                Status : 'open',
+                Date : '1'
             },{
                 Name : 'abc',
                 Qty : 1,
                 AvgPrice : '1$',
                 PL : 1,
-                Status : 'open'
+                Status : 'open',
+                Date : '1'
             },{
                 Name : 'abc',
                 Qty : 1,
                 AvgPrice : '1$',
                 PL : 1,
-                Status : 'open'
+                Status : 'open',
+                Date : '1'
             },{
                 Name : 'abc',
                 Qty : 1,
                 AvgPrice : '1$',
                 PL : 1,
-                Status : 'open'
-            }
-        ]
+                Status : 'open',
+                Date : '1'
+            },{
+                Name : 'abc',
+                Qty : 1,
+                AvgPrice : '1$',
+                PL : 1,
+                Status : 'open',
+                Date : '1'
+            },{
+                Name : 'abc',
+                Qty : 1,
+                AvgPrice : '1$',
+                PL : 1,
+                Status : 'open',
+                Date : '1'
+            },{
+                Name : 'abc',
+                Qty : 1,
+                AvgPrice : '1$',
+                PL : 1,
+                Status : 'open',
+                Date : '1'
+            },{
+                Name : 'abc',
+                Qty : 1,
+                AvgPrice : '1$',
+                PL : 1,
+                Status : 'open',
+                Date : '1'
+            },{
+                Name : 'abc',
+                Qty : 1,
+                AvgPrice : '1$',
+                PL : 1,
+                Status : 'open',
+                Date : '1'
+            },{
+                Name : 'abc',
+                Qty : 1,
+                AvgPrice : '1$',
+                PL : 1,
+                Status : 'open',
+                Date : '1'
+            },{
+                Name : 'abc',
+                Qty : 1,
+                AvgPrice : '1$',
+                PL : 1,
+                Status : 'open',
+                Date : '1'
+            },{
+                Name : 'abc',
+                Qty : 1,
+                AvgPrice : '1$',
+                PL : 1,
+                Status : 'open',
+                Date : '1'
+            },{
+                Name : 'abc',
+                Qty : 1,
+                AvgPrice : '1$',
+                PL : 1,
+                Status : 'open',
+                Date : '1'
+            },{
+                Name : 'abc',
+                Qty : 1,
+                AvgPrice : '1$',
+                PL : 1,
+                Status : 'open',
+                Date : '1'
+            },{
+                Name : 'abc',
+                Qty : 1,
+                AvgPrice : '1$',
+                PL : 1,
+                Status : 'open',
+                Date : '1'
+            },
+        ],
+        focusedData : null
     };
+    componentWillMount(){
+        if(this.state.maxPage == null){
+            this.state.maxPage = (this.state.fetchedData.length/5) - 1;
+        }
+    }
+    handlePagination = (val) => {
+        if(val=='First'){
+            this.setState({currentPage : 0});
+        } else if(val=='Prev') {
+            this.setState((prevState)=>({currentPage : prevState.currentPage - 1}));
+        } else if(val=='Next') {
+            this.setState((prevState)=>({currentPage : prevState.currentPage + 1}));
+        } else {
+            this.setState((prevState)=>({currentPage : prevState.maxPage}));
+        }
+    }
     handleHeaderButtons = (option) => this.setState(() => ({ page: option }))
     render() {
         return (
@@ -116,6 +226,36 @@ export default class OrderList extends React.Component{
                         />
                     }
                 </div>
+                <div className="OrderList--pagination">
+                    <button 
+                        className={(this.state.currentPage!==0)?"OrderList--paginationbutton":"OrderList--paginationbutton__selected"}
+                        disabled={this.state.currentPage===0} 
+                        onClick={()=> this.handlePagination('First')}
+                    >
+                        First
+                    </button>
+                    <button 
+                        className={(this.state.currentPage!==0)?"OrderList--paginationbutton":"OrderList--paginationbutton__selected"}
+                        disabled={this.state.currentPage===0} 
+                        onClick={()=> this.handlePagination('Prev')}
+                    >
+                        Previous
+                    </button>
+                    <button 
+                        className={(this.state.currentPage!==this.state.maxPage)?"OrderList--paginationbutton":"OrderList--paginationbutton__selected"}
+                        disabled={this.state.currentPage===this.state.maxPage} 
+                        onClick={()=> this.handlePagination('Next')}
+                    >
+                        Next
+                    </button>
+                    <button 
+                        className={(this.state.currentPage!==this.state.maxPage)?"OrderList--paginationbutton":"OrderList--paginationbutton__selected"}
+                        disabled={this.state.currentPage===this.state.maxPage} 
+                        onClick={()=> this.handlePagination('Last')}
+                    >
+                        Last
+                    </button>
+                </div>
             </div>
         )
     }
@@ -138,7 +278,7 @@ const OrderBody = (props) => (
             <tbody>
                 {
                     props.data.map((e,i)=>(
-                        <tr>
+                        <tr key={e+i}>
                             <td>{i+1}</td>
                             <td>{e.Name}</td>
                             <td>{e.Qty}</td>
